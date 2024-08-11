cbd2d75365d6ad20bdc4b543daa1e5761b804960
sijinyu <sijinyudev@gmail.com>
2024-08-11 20:04:13 +0900

    feat: setup api,querykeys, time, pxToRem base constans

---
+++ src/constants/api.ts
@@ -0,0 +1,5 @@
+export const ENDPOINT = {
+  LURE_DEALS: `/deals/lure-deal`,
+  BRAND_DELAS: `/deals/brand-deal`,
+  TIME_DELAS: `/deals/time-deal`,
+};
---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,5 @@
+export const queryKeys = {
+  LURE_DEALS: 'lure_deals',
+  BRNAD_DEALS: 'brand_deals',
+  TIME_DEALS: 'time_deals',
+};
