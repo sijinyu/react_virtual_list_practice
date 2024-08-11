09167e5825dcd37c39a0031dc6930b036a4ddfd6
sijinyu <sijinyudev@gmail.com>
2024-08-11 23:40:05 +0900

    refactor:  lureDealCard

--- src/components/specific/deals/timeDeal/LureDeal/LureDealCard.tsx
+++ src/components/specific/deals/timeDeal/LureDeal/LureDealCard.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
 import Card from '@/components/unit/Card';
 import { LureDeal } from '@/types';
+import { memo } from 'react';
 
 type LureDealCardProps = {
   deal: LureDeal;
 };
 
-const LureDealCard: React.FC<LureDealCardProps> = ({ deal }) => (
-  <Card className='w-146 h-227 rounded-12'>
-    <Card.Image src={deal.image} alt={deal.title} />
-    <Card.Content className='px-10 pt-9'>
-      <Card.Title title={deal.title} className='text-gray-800' />
-      <Card.Price
-        discountRate={deal.discountRate}
-        discountedPrice={deal.discountedPrice}
-        className='text-lg'
-      />
-    </Card.Content>
-  </Card>
+const LureDealCard = ({ deal }: LureDealCardProps) => (
+  <a className='cursor'>
+    <Card className='w-146 h-227 rounded-12'>
+      <Card.Image src={deal.image} alt={deal.title} />
+      <Card.Content className='px-10 pt-9'>
+        <Card.Title title={deal.title} className='text-gray-800' />
+        <Card.Price
+          discountRate={deal.discountRate}
+          discountedPrice={deal.discountedPrice}
+          className='text-lg'
+        />
+      </Card.Content>
+    </Card>
+  </a>
 );
 
-export default React.memo(LureDealCard);
+export default memo(LureDealCard);
